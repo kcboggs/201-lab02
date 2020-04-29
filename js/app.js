129999572c981f7dfd89128f26b32ce5ff78994e
kcboggs <kimberley.cabrera@yahoo.com>
2020-04-28 17:22:23 -0700

    3rdupdate

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
+'use strict'
 alert('Hello!');
 
 
 var question1 = prompt('Do you have any pets?')
 var response;
-console.log('');
+console.log(question1);
 
-  if (question1 === 'yes') {
+  if (question1.toLowerCase() === 'yes') {
       alert('Nice I do too');
   } else if (question1 === 'no') {
       alert('They are pretty cool to have');
@@ -13,8 +14,9 @@ console.log('');
     
 var question2 = prompt('Do you like coding?')
 var response;
+console.log(question2);
 
-  if (question2 === 'yes') {
+  if (question2.toUpperCase === 'Yes') {
       alert('Well your at the right place to learn!');
   } else if (question2 === 'no') {
       alert('No problem coding is not for everyone');
@@ -22,6 +24,7 @@ var response;
 
 var question3 = prompt('Do you live in Seattle?')
 var response;
+console.log(question3);
 
   if (question3 === 'yes') {
       alert('Hello neighbor');
@@ -31,18 +34,20 @@ var response;
 
 var question4 = prompt('Are you married?')
 var response;
+console.log(question4);
 
-  if (qestion4 === 'yes') {
+  if (question4 === 'yes') {
       alert('I am too');
   } else if (question4 === 'no') {
-      alert = ('Your not the only one!')
+      alert('Your not the only one!')
   }
+
 var question5 = prompt('Do you have kids?')
 var response;
-
+console.log(question5);
   if (question5 === 'yes') {
     alert('I also have kids');
-  } else if (qustion5 === 'no') {
+  } else if (question5 === 'no') {
     alert('Just asking');  
   }
     
