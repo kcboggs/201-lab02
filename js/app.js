0a08733d4159d68843cdd8077aba7c271346e760
kcboggs <kimberley.cabrera@yahoo.com>
2020-04-30 00:21:53 -0700

    fixes

--- js/app.js
+++ js/app.js
@@ -2,31 +2,27 @@
 
 alert('Hello!');
 var name = prompt('What is your name?');
-
-var question1 = prompt('Do you have any pets?')
-var response;
-console.log("prompt for question1")
-
-
-  if (question1.toLowerCase() === 'yes') {
-      alert('Nice, I do too!');
-  } else if (question1 === 'no') {
-      alert('They are pretty cool to have!');
-  }
+console.log('visitor name is' + name);
     
-var question2 = prompt('Do you like coding?')
-var response;
-console.log(question2);
+var question1 = prompt('Do you like coding?')
+console.log(question1);
 
-  if (question2 === 'yes') {
+  if (question1 === 'yes') {
       alert('Well your at the right place to learn!');
+  } else if (question1 === 'no') {
+      alert('No problem coding is not for everyone!');
+  }
+
+  var question2 = prompt('Do you have any pets?')
+  console.log("question2")
+    
+  if (question2.toLowerCase() === 'yes') {
+        alert('Nice, I do too!');
   } else if (question2 === 'no') {
-     alert('No problem coding is not for everyone!');
-  
-    }
+        alert('They are pretty cool to have!');
+  }
 
 var question3 = prompt('Do you live in Seattle?')
-var response;
 console.log(question3);
 
   if (question3 === 'yes') {
@@ -36,7 +32,6 @@ console.log(question3);
   }
 
 var question4 = prompt('Are you married?')
-var response;
 console.log(question4);
 
   if (question4 === 'yes') {
@@ -46,11 +41,29 @@ console.log(question4);
   }
 
 var question5 = prompt('Do you have kids?')
-var response;
-console.log(question5);
+console.log(question5); 
+
   if (question5 === 'yes') {
-    alert('I also have kids!');
+      alert('I also have kids!');
   } else if (question5 === 'no') {
-    alert('Just asking!');  
+      alert('Just asking!');  
   }
-    
+
+var question6 = prompt('What number am I thinking of?');
+  
+  if (question6 > 15) {
+      prompt('too high try again');
+  } else if (question6 < 15) {
+      prompt('too low try again');  
+  } else if(question6 === 15) {
+      alert('you guessed correctly!');
+  }
+
+var question7 =prompt('Where do rainbows come from?');
+var answer;('my backyard')
+
+  if (question7 != 'my backyard') {
+      prompt('take another guess');
+  } else if(question7 == 'my backyard') {
+      alert('hmm! sure why not'); 
+  } 
